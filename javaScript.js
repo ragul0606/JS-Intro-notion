7ddedf1125121c4644a93d2d015aacbae9487c8e
ragul0606 <ragulram0606@gmail.com>
2023-05-27 09:47:09 +0530

    Problem 1, Notion Done

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+// notion exercise
+
+// values & variables
+
+let country = "India";
+let continent = "Asia";
+let population = "145";
+
+console.log(country);
+console.log(continent);
+console.log(population);
