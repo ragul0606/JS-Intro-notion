d0299bf6225adcc813762a058742610e71e612a1
ragul0606 <ragulram0606@gmail.com>
2023-05-27 10:15:01 +0530

    Basic 3 done

--- javaScript.js
+++ javaScript.js
@@ -6,6 +6,27 @@ let country = "India";
 let continent = "Asia";
 let population = "145";
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// data types
+
+// const isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
+
+// let, const & var
+
+const language = "tamil";
+// language = "English";
+// console.log(language);
+
+// basic operators
+
+let halfOfPopulation = population / 2;
+console.log(halfOfPopulation);
