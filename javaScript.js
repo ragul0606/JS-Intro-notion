43482c2119c25177b81dff4924088de4a08628eb
ragul0606 <ragulram0606@gmail.com>
2023-05-27 12:34:26 +0530

    coding challenge 2 completed

--- javaScript.js
+++ javaScript.js
@@ -4,7 +4,7 @@
 
 let country = "India";
 let continent = "Asia";
-let population = 145;
+let population = 140;
 const language = "tamil";
 
 // console.log(country);
@@ -53,3 +53,66 @@ const language = "tamil";
 // console.log(description);
 
 // Coding Challenge #1
+
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+// let markWeight = 95;
+// let markHeight = 1.88;
+// let johnWeight = 85;
+// let johnHeight = 1.76;
+
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
+
+// console.log(markBMI.toFixed(1));
+// console.log(johnBMI.toFixed(1));
+
+// let markHigherBMI = markBMI > johnBMI;
+
+// console.log(markHigherBMI);
+
+// string & template literals
+
+// console.log(
+//   `Mark's BMI is ${markBMI.toFixed(2)} and John's BMI is ${johnBMI.toFixed(
+//     2
+//   )} and so it is ${markHigherBMI}`
+// );
+
+// if/else
+
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(`${country}'s population is below average`);
+// }
+
+// Coding Challenge #2
+
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
+
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
+
+// console.log(markBMI.toFixed(1));
+// console.log(johnBMI.toFixed(1));
+
+// if (markBMI > johnBMI) {
+//   console.log(
+//     `Mark's BMI (${markBMI.toFixed(
+//       2
+//     )}) is higher than John's (${johnBMI.toFixed(2)})!`
+//   );
+// } else {
+//   console.log(
+//     `John's BMI (${johnBMI.toFixed(2)})is higher than Marks's (${markBMI})! `
+//   );
+// }
